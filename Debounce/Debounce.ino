186e3aba5e5589d77341b346fbd2dfdae0cb6bd4
Una LEstrange <ulestrange@e2014-01.campus.itsligo.ie>
2017-02-22 12:19:26 +0000

    changed pin numbers and debounce delay

--- Debounce/Debounce.ino
+++ Debounce/Debounce.ino
@@ -29,8 +29,8 @@
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 2;    // the number of the pushbutton pin
-const int ledPin = 13;      // the number of the LED pin
+const int buttonPin = 5;    // the number of the pushbutton pin
+const int ledPin = 6;      // the number of the LED pin
 
 // Variables will change:
 int ledState = HIGH;         // the current state of the output pin
@@ -40,7 +40,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
+long debounceDelay = 500;    // the debounce time; increase if the output flickers
 
 void setup() {
   pinMode(buttonPin, INPUT);
